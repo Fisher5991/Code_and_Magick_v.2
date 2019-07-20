29378277217d09e7f4ed29c20ac3b3a8e50bd123
Fisher5991 <fisher5991@gmail.com>
2019-07-20 18:25:23 +0300

    Учебный проект: pimp my mage

--- js/setup.js
+++ js/setup.js
@@ -2,24 +2,16 @@
 
 (function () {
   var WIZARDS_NUMBER = 4;
-  var wizardsNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф',
-  'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var wizardsSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко',
-  'Топольницкая', 'Нионго', 'Ирвинг'];
-  var coatsColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-
-  var wizards = [];
+  var URL = 'https://js.dump.academy/code-and-magick';
 
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
   var setupWizardFormElement = setup.querySelector('.setup-wizard-form');
-  var setupUserName = setupWizardFormElement.querySelector('.setup-user-name');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template');
   var setupSimilar = document.querySelector('.setup-similar');
   var setupSimilarList = document.querySelector('.setup-similar-list');
+  var setupForm = setup.querySelector('.setup-wizard-form');
 
   var setupPlayer = document.querySelector('.setup-player');
   var wizardCoat = setupPlayer.querySelector('.wizard-coat');
@@ -33,67 +25,24 @@
     'ENCTYPE': 'multipart/form-data'
   }
 
-  var generateWizards = function (quantity) {
-    var wizardsNamesCopy = wizardsNames.slice();
-    var wizardsSurnamesCopy = wizardsSurnames.slice();
-    var coatsColorsCopy = coatsColors.slice();
-    var eyesColorsCopy = eyesColors.slice();
+  var addWizards = function (wizard) {
+    var similarItem = similarWizardTemplate.content.cloneNode(true);
+    similarItem.querySelector('.setup-similar-label').textContent = wizard.name;
+    similarItem.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    similarItem.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
-    for (var i = 0; i < quantity; i++) {
-      var wizard = {};
-      var temporaryName = [];
-      var randomNumber = window.utils.generateNumber(0, wizardsNamesCopy.length - 1);
-
-      temporaryName.push(wizardsNamesCopy[randomNumber]);
-      wizardsNamesCopy.splice(randomNumber, 1);
-      randomNumber = window.utils.generateNumber(0, wizardsSurnamesCopy.length - 1);
-      temporaryName.push(wizardsSurnamesCopy[randomNumber]);
-      wizardsSurnamesCopy.splice(randomNumber, 1);
-
-      randomNumber = window.utils.generateNumber(0, temporaryName.length - 1);
-      wizard.name = temporaryName[randomNumber] + ' ';
-      temporaryName.splice(randomNumber, 1);
-      wizard.name += temporaryName[0];
-
-      randomNumber = window.utils.generateNumber(0, coatsColorsCopy.length - 1);
-      wizard.coatColor = coatsColorsCopy[randomNumber];
-      coatsColorsCopy.splice(randomNumber, 1);
-
-      randomNumber = window.utils.generateNumber(0, eyesColorsCopy.length - 1);
-      wizard.eyesColor = eyesColorsCopy[randomNumber];
-      eyesColorsCopy.splice(randomNumber, 1);
-
-      wizards.push(wizard);
-    }
-  };
-
-  var addWizards = function () {
-    for (var i = 0; i < WIZARDS_NUMBER; i++) {
-      var similarItem = similarWizardTemplate.content.cloneNode(true);
-      similarItem.querySelector('.setup-similar-label').textContent = wizards[i].name;
-      similarItem.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-      similarItem.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-      similarItemFragment.appendChild(similarItem);
-    }
-  }
-
-  var onPopupEscPress = function (evt) {
-    var resultEscPressAction = window.utils.isEscEvent(evt);
-
-    if (resultEscPressAction && (evt.target !== setupUserName)) {
-      window.togglePopup.close(evt, setup, onPopupEscPress);
-    }
+    return similarItem;
   }
 
   var onSetupOpenClick = function (evt) {
-    window.togglePopup.open(evt, setup, onPopupEscPress);
+    window.togglePopup.open(evt, setup);
   }
 
   var onSetupOpenKeydown = function (evt) {
     var resultOpenAction = window.utils.isEnterEvent(evt);
 
     if (resultOpenAction) {
-      window.togglePopup.open(evt, setup, onPopupEscPress);
+      window.togglePopup.open(evt, setup);
     }
   }
 
@@ -101,13 +50,13 @@
     var resultCloseAction = window.utils.isEnterEvent(evt);
 
     if (resultCloseAction) {
-      window.togglePopup.close(evt, setup, onPopupEscPress);
+      window.togglePopup.close();
     }
   }
 
   var onSetupCloseClick = function (evt) {
     evt.preventDefault();
-    window.togglePopup.close(evt, setup, onPopupEscPress);
+    window.togglePopup.close();
   }
 
   var onWizardCoatClick = function (evt) {
@@ -122,6 +71,35 @@
     evt.target.style.backgroundColor = window.colorizeElement('fireball');
   }
 
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  }
+
+  var onSetupFormSubmit = function (evt) {
+    window.backend.save(new FormData(setupForm), window.togglePopup.close, errorHandler);
+    evt.preventDefault();
+  }
+
+  window.loadWizardData = function (wizards) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < WIZARDS_NUMBER; i++) {
+      fragment.appendChild(addWizards(wizards[i]));
+    }
+
+    setupSimilarList.appendChild(fragment);
+
+    setupSimilar.classList.remove('hidden');
+  }
+
   setupOpen.tabIndex = '0';
   setupClose.tabIndex = '0';
   setupOpen.addEventListener('click', onSetupOpenClick);
@@ -129,11 +107,6 @@
   setupClose.addEventListener('click', onSetupCloseClick);
   setupClose.addEventListener('keydown', onSetupCloseKeydown);
 
-  generateWizards(WIZARDS_NUMBER);
-  addWizards();
-
-  setupSimilarList.appendChild(similarItemFragment);
-  setupSimilar.classList.remove('hidden');
   setupWizardFormElement.action = wizardForm.ACTION;
   setupWizardFormElement.method = wizardForm.METHOD;
   setupWizardFormElement.enctype = wizardForm.ENCTYPE;
@@ -141,4 +114,9 @@
   wizardCoat.addEventListener('click', onWizardCoatClick);
   wizardEyes.addEventListener('click', onWizardEyesClick);
   wizardFireball.addEventListener('click', onWizardFireballClick);
+
+  // window.backend.load(loadWizardData, errorHandler);
+  window.backend.getJSONP('loadWizardData');
+
+  setupForm.addEventListener('submit', onSetupFormSubmit);
 })();
